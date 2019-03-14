09e5593e58d8ad6191f60944db8ce94331dd85cd
Joshua Noronha <joshua.noronha13@gmail.com>
2019-03-15 02:22:55 +0530

    User management done

--- app.js
+++ app.js
@@ -2,74 +2,225 @@ var express = require("express"),
 	bodyParser = require("body-parser"),
 	mongoose = require("mongoose"),
 	app = express(),
-	// flash = require("connect-flash-plus"),
+	methodOverride = require("method-override"),
+	flash = require("connect-flash-plus"),
 	passport = require("passport"),
-	passportLocal=require("passport-local").Strategy,
+	passportLocal = require("passport-local").Strategy,
 	// passportFacebook = require("passport-facebook").Strategy,
-    // passportGoogle = require('passport-google-oauth').OAuth2Strategy,
+	// passportGoogle = require('passport-google-oauth').OAuth2Strategy,
 	// configAuth = require("./auth.js"),
-	// User = require("./model/user.js"),
+	User = require("./model/student.js"),
 	// Comment = require("./model/comments.js"),
 	// Pet = require("./model/pet.js"),
 	// Story = require("./model/story.js"),
 	// seedDB = require('./seedDB'),
 	session = require("express-session");
-	// methodOverride = require("method-override");
+// methodOverride = require("method-override");
+setTimeout(() => { mongoose.connect('mongodb://mongo/CrescendoHack') }, 1000)
 
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(methodOverride("_method"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + "/public"));
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 // app.use(cookieParser());
+app.use(session({
+	secret: "JoshuaEdwinAkash",
+	resave: false,
+	saveUninitialized: false
+}));
+app.locals.council = null;
+app.locals.admin = null;
 
-
-setTimeout(() => {mongoose.connect('mongodb://mongo/CrescendoHack')}, 1000)
-// app.use(cors());
+app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
-// app.use(methodOverride("_method"));
+passport.use('local-signup', new passportLocal({  // by default, local strategy uses username and password, we will override with email
+	usernameField: 'username',
+	passwordField: 'password',
+	passReqToCallback: true // allows us to pass back the entire request to the callback
+},
+	function (req, username, password, done) {
+		console.log("Reached here")
+		process.nextTick(function () {
+			// find a user whose email is the same as the forms email
+			// we are checking to see if the user trying to login already exists
+			User.findOne({ 'local.username': username }, function (err, user) {
+				// if there are any errors, return the error
+				if (err) {
+					console.log("Super Screwed")
+					return done(err);
+				}
+
+				// check to see if theres already a user with that email
+				if (user) {
+					console.log("Screwed")
+					return done(null, false, req.flash('error', 'That username is already taken.'));
+				} else {
+
+					// if there is no user with that email
+					// create the user
+					var newUser = new User();
+					// console.log(newUser.local)
+					// set the user's local credentials
+					// newUser.local.username = username;
+					// newUser.local.firstname = req.body.fname;
+					// newUser.local.lastname = req.body.lname;
+					newUser.username = username;
+					newUser.password = newUser.generateHash(password);
+
+					// save the user
+					newUser.save(function (err) {
+						if (err) {
+							console.log("Screwed")
+							throw err;
+						}
+						console.log("Registered new User")
+						app.locals.username = username
+						return done(null, newUser, req.flash("sucess", "Welcome to CRCE,you have been registered sucessfully"));
+					});
+				}
+
+			});
+
+		});
+
+	}
+
+));
+
+passport.use('local-login', new passportLocal({
+	// by default, local strategy uses username and password, we will override with email
+	usernameField: 'username',
+	passwordField: 'password',
+	passReqToCallback: true // allows us to pass back the entire request to the callback
+},
+	function (req, username, password, done) { // callback with email and password from our form
+		console.log("Reached"+username+password)
+		// find a user whose email is the same as the forms email
+		// we are checking to see if the user trying to login already exists
+		User.findOne({ 'username': username }, function (err, user) {
+			// if there are any errors, return the error before anything else
+		// console.log("Could have found 1")			
+			if (err)
+			{
+				console.log(err)
+				return done(err);
+			}
+
+			// if no user is found, return the message
+			if (!user)
+			{
+				console.log("No user")
+				return done(null, false, req.flash('error', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
+			}
+
+			// if the user is found but the password is wrong
+			console.log(password)
+			if (!user.validPassword(password))
+			{
+				// Console.log("Bad pass")
+				return done(null, false, req.flash('error', 'Invalid password.')); // create the loginMessage and save it to session as flashdata
+			}
+			// console.log("SHIT")
+			// all is well, return successful user
+			console.log(user.council)
+			if (user.council)
+			{
+				console.log("In here")
+				app.locals.council = user.council;
+			}
+			if (user.admin)
+			{
+				app.locals.admin = user.admin;
+			}
+			app.locals.username = username
+			return done(null, user);
+		});
+
+	}));
+passport.serializeUser(function (user, done) {
+	done(null, user.id);
+});
+passport.deserializeUser(function (id, done) {
+	User.findById(id, function (err, user) {
+		done(err, user);
+	});
+});
+app.use(function(req,res,next){
+	res.locals.currentUser = req.user;
+	res.locals.error = req.flash("error");
+	res.locals.sucess = req.flash("sucess");
+	next();
+});
+
+// app.use(cors());
 
 
-app.get("/councils",function(req,res){
+
+app.get("/councils", function (req, res) {
 	// res.send("HElp")
 	res.render("councils")
 })
 
-app.get("/events",function(req,res){
+app.get("/events", function (req, res) {
 	// res.send("HElp")
 	res.render("events")
 })
 
-app.get("/login",function(req,res){
+app.get("/login", function (req, res) {
 	// res.send("HElp")
 	res.render("login")
 })
 
-app.get("/signup",function(req,res){
+app.get("/signup", function (req, res) {
 	// res.send("HElp")
 	res.render("signup")
 })
 
-app.get("/event_details",function(req,res){
+app.get("/event_details", function (req, res) {
 	// res.send("HElp")
 	res.render("event_expanded")
 })
 
 
-app.get("/review",function(req,res){
+app.get("/review", function (req, res) {
 	res.render("review")
 })
 
-app.get("/",function(req,res){
+app.get("/", function (req, res) {
 	res.render("index")
 })
 
-app.get("*",function(req,res){
+
+app.post('/signup', passport.authenticate('local-signup', {
+	successRedirect: '/', // redirect to the secure profile section
+	failureRedirect: '/signup', // redirect back to the signup page if there is an error
+	failureFlash: true, // allow flash messages
+	successFlash: true
+}));
+
+app.post('/login', passport.authenticate('local-login', {
+	successRedirect: '/', // redirect to the secure  profile section
+	failureRedirect: '/login', // redirect back to the signup page if there is an error
+	failureFlash: true, // allow flash messages
+	successFlash: true
+}));
+app.get("/logout", function (req, res) {
+	req.logout();
+	app.locals.username = null;
+	app.locals.admin = null;
+	app.locals.council = null;
+
+	res.redirect("/");
+});
+app.get("*", function (req, res) {
 	res.send("<h1>404 Page Not Found<h1>");
 });
 
 app.listen(3000, function () {
-  console.log('Ready');
+	console.log('Ready');
 });
 
 module.exports = app;
--- model/student.js
+++ model/student.js
@@ -6,6 +6,8 @@ var student = new mongoose.Schema({
 	name: String,
 	username: String,
 	password: String,
+	council: Boolean,
+	admin: Boolean,
 	email: String,
 	phone: Number,
 	resume: {
@@ -100,16 +102,12 @@ var student = new mongoose.Schema({
 }
 );
 
-
-student.methods.generateHash = function (password) {
-	return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+student.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 };
-
-// checking if password is valid
-student.methods.validPassword = function (password) {
-	return bcrypt.compareSync(password, this.local.password);
+student.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
 };
 
 
-
-module.exports = mongoose.model("Student", student);
+module.exports = mongoose.model("User", student);
